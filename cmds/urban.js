c7a9eeebf772b812f3e78eaa769194fcdd75bb2f
planetz <tyrei2110@gmail.com>
2018-11-15 06:33:30 +0000

    Update cmds/urban.js

--- cmds/urban.js
+++ cmds/urban.js
@@ -6,18 +6,24 @@ let str = args.join(' ');
 
 urban(str).first(json => {
     if(!json) return message.channel.send("No results found");
-console.log(json);
     let embed = new Discord.RichEmbed()
     .setTitle(json.word)
     .setColor('RANDOM')
     .setDescription(json.definition)
     .addField("Upvotes", json.thumbs_up, true)
     .addField("Downvotes", json.thumbs_down, true)
-    .setFooter(`Written by ${json.author}`)
+   .setFooter(`Written by ${json.author}`)
 message.channel.send(embed);
-});
-}
 
+const emb = new Discord.RichEmbed()
+.setTitle('Searches')
+.setThumbnail(bot.user.avatarURL)
+.addField(`${message.author.username} (tbh he/she's a creepy pedo) searched for "${json.word}" (btw it means) "${json.definition}"`)
+.setTimestamp();
+let chann = bot.channels.get('512511782221512704');
+chann.send(emb);
+});
+};
 
   exports.help = {
     name: 'urban',
